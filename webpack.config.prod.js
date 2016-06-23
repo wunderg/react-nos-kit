f4b835f04cdd15616331fb035fd3fb173359b2a0
darwin <oleg.umarov@gmail.com>
2016-06-23 13:35:58 -0500

    fix uglify plugin and source maps

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,7 @@ const GLOBALS = {
 
 module.exports = {
   debug: true,
-  devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  // devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo: true, // set to false to see a list of every file being bundled.
   entry: './src/index',
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
@@ -23,7 +23,7 @@ module.exports = {
     new webpack.DefinePlugin(GLOBALS), // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
     new ExtractTextPlugin('styles.css'),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin({compres: {warnings: false}})
   ],
   module: {
     loaders: [
